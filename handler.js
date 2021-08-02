e706381e961af12a1b0436acf2d23d0bcdf8e395
en-chang <enchangcs@gmail.com>
2021-08-01 22:19:10 -0500

    Fix logical error for rankEmoji

--- handler.js
+++ handler.js
@@ -6,7 +6,7 @@ const emojis = [
 
 module.exports.rank = async (event) => {
   const rank = event.queryStringParameters.rank
-  const rankEmoji = emojis[rank > emojis.length ? emojis.length - 1 : rank];
+  const rankEmoji = emojis[rank >= emojis.length ? emojis.length - 1 : rank];
   return {
     statusCode: 200,
     headers: {

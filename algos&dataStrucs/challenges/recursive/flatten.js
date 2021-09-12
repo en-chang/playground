b9cfbf7d05b948e15854c996a9e90282c289bece
en-chang <enchangcs@gmail.com>
2021-09-12 10:10:49 -0700

    Complete recursion challenges

--- algos&dataStrucs/challenges/recursive/flatten.js
+++ algos&dataStrucs/challenges/recursive/flatten.js
@@ -1,9 +1,30 @@
-function flatten(arr){
-  // add whatever parameters you deem necessary - good luck!
-  console.log(arr);
+// create new array
+// loop through oldArr
+// if first val of array !Array.isArray(oldArr[0])
+  // newArr.push(val)
+// else meaning the first val is an array
+  // flatten(oldArr[0])
+// loop finishes
+// return newArr
+
+function flatten(oldArr) {
+  let newArr = [];
+
+  function recursion(oldArr) {
+    for (let index in oldArr) {
+      if (!Array.isArray(oldArr[index])) {
+        newArr.push(oldArr[index]);
+      }
+      recursion(oldArr[index]);
+    }
+  }
+
+  recursion(oldArr);
+
+  return newArr;
 }
 
 console.log(flatten([1, 2, 3, [4, 5] ])); // [1, 2, 3, 4, 5]
 console.log(flatten([1, [2, [3, 4], [[5]]]])); // [1, 2, 3, 4, 5]
 console.log(flatten([[1],[2],[3]])); // [1,2,3]
-console.log(flatten([[[[1], [[[2]]], [[[[[[[3]]]]]]]]]])); // [1,2,3
+console.log(flatten([[[[1], [[[2]]], [[[[[[[3]]]]]]]]]])); // [1,2,3]
--- algos&dataStrucs/challenges/recursive/nestedEvenSum.js
+++ algos&dataStrucs/challenges/recursive/nestedEvenSum.js
@@ -1,7 +1,32 @@
-// I have no idea what this one is asking me to do
+// GOAL
+// sum of all even numbers in an object including nested
+// PSUEDO
+// create empty sum variable
+// loop through object
+// if value is a number type and even
+  // add to sum variable
+// if value is an object
+  // recursion
+// loop complete
+// return sum variable
 
-function nestedEvenSum (obj) {
+function nestedEvenSum(obj) {
   // add whatever parameters you deem necessary - good luck!
+  let evenSum = 0;
+  
+  function recursion(obj) {
+    for (let key in obj) {
+      if (typeof obj[key] === 'number' && obj[key] % 2 === 0) {
+        evenSum += obj[key];
+      } else if (typeof obj[key] === 'object') {
+        recursion(obj[key]);
+      }
+    }
+  }
+
+  recursion(obj);
+
+  return evenSum;
 }
 
 var obj1 = {

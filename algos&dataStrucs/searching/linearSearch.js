478ec95e30d4d041427f15fd32682d142b977b53
en-chang <enchangcs@gmail.com>
2021-10-04 13:34:00 -0500

    Reset playground

--- algos&dataStrucs/searching/linearSearch.js
+++
@@ -1,9 +0,0 @@
-function linearSearch(arr, num){
-  // add whatever parameters you deem necessary - good luck!
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === num) {
-      return i;
-    }
-  }
-  return -1;
-}
